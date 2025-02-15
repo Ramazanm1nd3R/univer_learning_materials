04531fe8ce59ece296354a315e9434ed51bbc59c
Ramazan <ospanr4@gmail.com>
2025-02-16 01:18:26 +0500

    add 4th oop lab hw

--- Java_OOP/Lab4/Lab4/src/labtasks/homework/task2_search/MainHomework2.java
+++ Java_OOP/Lab4/Lab4/src/labtasks/homework/task2_search/MainHomework2.java
@@ -1,4 +1,28 @@
 package labtasks.homework.task2_search;
+import labtasks.homework.task1_books.books.*;
+import java.util.Scanner;
 
 public class MainHomework2 {
+    public static void main(String[] args) {
+        Book[] books = new Book[10];
+
+        // тестовые кейсы книг из предыдущего main класса
+
+        books[0] = new ScientificBook("Physics", "S101", 320, 15000, "Oxford");
+        books[1] = new ScientificBook("Chemistry", "S102", 280, 13000, "Cambridge");
+        books[2] = new ScientificBook("Biology", "S103", 300, 14000, "Harvard");
+        books[3] = new ScientificBook("Math", "S104", 400, 16000, "MIT");
+        books[4] = new ScientificBook("Astronomy", "S105", 350, 17000, "NASA");
+
+
+        books[5] = new LiteratureBook("To Kill a Mockingbird", "TK009", 281, "Harper Lee", 1960);
+        books[6] = new LiteratureBook("War and Peace", "L201", 1225, "Tolstoy", 1869);
+        books[7] = new LiteratureBook("The Great Gatsby", "L203", 218, "Fitzgerald", 1925);
+        books[8] = new LiteratureBook("Moby Dick", "L204", 635, "Melville", 1851);
+        books[9] = new LiteratureBook("1984", "N1984", 328, "George Orwell", 1949);
+
+        Scanner sc = new Scanner(System.in);
+        SearchMenu searcher = new SearchMenu(sc, books);
+        searcher.itemSearch();
+    }
 }
--- Java_OOP/Lab4/Lab4/src/labtasks/homework/task2_search/SearchMenu.java
+++ Java_OOP/Lab4/Lab4/src/labtasks/homework/task2_search/SearchMenu.java
@@ -1,4 +1,100 @@
 package labtasks.homework.task2_search;
+import labtasks.homework.task1_books.books.Book;
+
+import java.util.Scanner;
 
 public class SearchMenu {
+    private Scanner input;
+    private Book[] books;
+
+    public SearchMenu(Scanner input, Book[] books) {
+        this.input = input;
+        this.books = books;
+    }
+
+    public void itemSearch() {
+        while (true) {
+            System.out.println("\nPRESS [1] TO SEARCH BOOK BY NAME");
+            System.out.println("PRESS [2] TO SEARCH BOOK BY CODE");
+            System.out.println("PRESS [3] TO SEARCH BOOK BY PAGES AMOUNT");
+            System.out.println("PRESS [0] EXIT");
+
+            int choice = input.nextInt();
+            input.nextLine(); // ОЧИЩАЕМ буфер
+
+            if (choice == 1) searchByName();
+            else if (choice == 2) searchByCode();
+            else if (choice == 3) searchByPagesRange();
+            else if (choice == 0) break;
+            else System.out.println("Invalid choice, try again.");
+        }
+    }
+
+
+    public void searchByName() {
+        System.out.print("INSERT NAME OF THE BOOK: ");
+        String searchName = input.nextLine().trim();
+
+
+        boolean found = false;
+        for (int i = 0; i < books.length; i++) {
+            if (books[i] != null && books[i].getName().equalsIgnoreCase(searchName)) {
+                System.out.println(books[i].getBookData());
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.println("Book not found");
+        }
+    }
+
+    public void searchByCode() {
+        System.out.print("INSERT CODE OF THE BOOK: ");
+        String searchCode = input.nextLine().trim();
+
+        boolean found = false;
+        for (int i = 0; i < books.length; i++) {
+            if (books[i] != null && books[i].getCode().equalsIgnoreCase(searchCode)){
+                System.out.println(books[i].getBookData());
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.println("Book not found");
+        }
+    }
+
+    public void searchByPagesRange() {
+        System.out.println("INSERT MINIMUM PAGES OF THE BOOK: ");
+        String minInput = input.nextLine().trim();
+        if (!minInput.matches("\\d+")) {
+            System.out.println("Not a number. Please enter a valid number.");
+            return;
+        }
+        Integer minPages = Integer.parseInt(minInput);
+
+        System.out.println("INSERT MAXIMUM PAGES OF THE BOOK: ");
+        String maxInput = input.nextLine().trim();
+        if (!maxInput.matches("\\d+")) {
+            System.out.println("Not a number. Please enter a valid number.");
+            return;
+        }
+        Integer maxPages = Integer.parseInt(maxInput);
+
+        if (minPages < 0 || maxPages < 0 || minPages > maxPages) {
+            System.out.println("Invalid range, try again.");
+            return;
+        }
+
+        boolean found = false;
+        for (Book book : books) {
+            if (book != null && book.getPages() >= minPages && book.getPages() <= maxPages) {
+                System.out.println(book.getBookData());
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.println("No books found in the specified range.");
+        }
+    }
 }
