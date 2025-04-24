ba7ba5366cd4a641fd818a50dc7155db0e0649ab
Ramazan <ospanr4@gmail.com>
2025-04-24 15:58:07 +0500

    9th lab small change

--- Java_OOP/Lab9/Lab9/src/server/ServerApp_2Task.java
+++ Java_OOP/Lab9/Lab9/src/server/ServerApp_2Task.java
@@ -1,14 +1,7 @@
 package server;
 
-import model.PackageData;
-import model.Book;
-import util.BookFileUtil;
-
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
 
 public class ServerApp_2Task {
     public static void main(String[] args) {
